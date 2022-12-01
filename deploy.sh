1b1b999a25f4d80faa60670f2bd4af3d9584db1c
Thiruppathi K <thiruppathi.k@thoughtworks.com>
2022-12-01 16:16:37 +0530

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Build for the target
+cargo build --target=aarch64-unknown-linux-gnu
+
+# Copy binary to Raspberry Pi. Replace the Ip with actual Ip.
+scp -r target/aarch64-unknown-linux-gnu/debug/rpi_sample pi@"$1":/home/pi/test/
---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use std::error::Error;
+use std::thread;
+use std::time::Duration;
+
+use rppal::gpio::Gpio;
+use rppal::system::DeviceInfo;
+
+// Gpio uses BCM pin numbering. BCM GPIO 23 is tied to physical pin 16.
+const GPIO_LED: u8 = 23;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
+
+    let mut pin = Gpio::new()?.get(GPIO_LED)?.into_output();
+
+    // Blink the LED by setting the pin's logic level high for 1000ms.
+    loop {
+        thread::sleep(Duration::from_millis(1000));
+        pin.set_high();
+        thread::sleep(Duration::from_millis(1000));
+        pin.set_low();
+    }
+}
