6cd1173344f36d21924f49003cb470a309884717
Thiruppathi K <thiruppathi.k@thoughtworks.com>
2022-12-05 18:46:41 +0530

    Blink LED based on PIR input

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,33 @@
 use std::error::Error;
-use std::thread;
-use std::time::Duration;
 
 use rppal::gpio::Gpio;
 use rppal::system::DeviceInfo;
 
-// Gpio uses BCM pin numbering. BCM GPIO 23 is tied to physical pin 16.
+// Gpio uses BCM pin numbering.
+// BCM GPIO 23 is tied to physical pin 16.
 const GPIO_LED: u8 = 23;
 
+// BCM GPIO 16 is tied to physical pin 36.
+const GPIO_PIR: u8 = 16;
+
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
+    println!("Blinking LED based on PIR input in a {}.", DeviceInfo::new()?.model());
 
-    let mut pin = Gpio::new()?.get(GPIO_LED)?.into_output();
+    // Set pin 23 as output pin
+    let mut pin_led = Gpio::new()?.get(GPIO_LED)?.into_output();
 
-    // Blink the LED by setting the pin's logic level high for 1000ms.
+    // Set pin 16 as input pin
+    let pin_pir = Gpio::new()?.get(GPIO_PIR)?.into_input();
+
+    pin_led.set_low();
+
     loop {
-        thread::sleep(Duration::from_millis(1000));
-        pin.set_high();
-        thread::sleep(Duration::from_millis(1000));
-        pin.set_low();
+        // Read PIR data and toggle the LED based on the input
+        if pin_pir.is_high() {
+            pin_led.set_high();
+            // println!("Motion detected");
+        } else {
+            pin_led.set_low();
+        }
     }
 }
